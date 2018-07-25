1d276ca4e404b2c5cf949a054a0e700b82ccd41d
Kevin <ketsang12@gmail.com>
2018-07-25 11:38:14 -0400

    2nd component finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import EmotionSelect from './components/EmotionSelect';
+import ActionDropdown from './components/ActionDropdowns';
 
 export default function App(props) {
   return (
-    <EmotionSelect />
+    <div>
+      <EmotionSelect />
+      <ActionDropdown />
+    </div>
   );
 }
--- src/components/ActionDropdowns.js
+++ src/components/ActionDropdowns.js
@@ -1,1 +1,25 @@
 import React from 'react';
+
+export default function ActionDropdown (props){
+    return (
+        <div className='ActionDropdown'>
+            <select className='happyAct'> 
+                <option value='clapHappy'>Clap Your Hands!</option>
+                <option value='shoutHappy'>Shout Hooray!</option>
+                <option value='jumpHappy'>Jump for Joy!</option>
+            </select>
+
+            <select className='sadAct'> 
+                <option value='crySad'>Cry Boo-Hoo</option>
+                <option value='frownSad'>Show a Frown</option>
+                <option value='nappySad'>Take a Nap</option>
+            </select>
+
+            <select className='angryAct'> 
+                <option value='tantrumAngry'>Throw Your Stuff!!</option>
+                <option value='yellAngry'>Scream and SHOUT!!</option>
+                <option value='slamAngry'>Slam the Door!!</option>
+            </select>
+        </div>
+    );
+}
