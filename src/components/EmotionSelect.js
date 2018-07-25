39428ece7b899e953c2e121b829a297e1cdc84d2
Jennifer Colna <jennifer.colna@gmail.com>
2018-07-25 11:25:40 -0400

    first component

---
+++ src/components/EmotionSelect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function EmotionSelect(props) {
+  return (
+    <div className='buttons'>
+      <button className='happy'>Happy</button>
+      <button className='sad'>Sad</button>
+      <button className='angry'>Angry</button>
+    </div>
+  );
+}
