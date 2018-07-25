1a5d051129731b4806ccf36250ac21536ca7ded3
Jennifer Colna <jennifer.colna@gmail.com>
2018-07-25 12:01:20 -0400

    gif display

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import EmotionSelect from './components/EmotionSelect';
 import ActionDropdown from './components/ActionDropdowns';
+import GifDisplay from './components/GifDisplay';
 
 export default function App(props) {
   return (
     <div>
       <EmotionSelect />
       <ActionDropdown />
+      <GifDisplay />
     </div>
   );
 }
--- src/components/ActionDropdowns.js
+++ src/components/ActionDropdowns.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
 export default function ActionDropdown (props){
-    return (
-        <div className='ActionDropdown'>
-            <select className='happyAct'> 
-                <option value='clapHappy'>Clap Your Hands!</option>
-                <option value='shoutHappy'>Shout Hooray!</option>
-                <option value='jumpHappy'>Jump for Joy!</option>
-            </select>
+  return (
+    <div className='ActionDropdown'>
+      <select className='happyAct'>
+        <option value='0'>Select an Action!</option> 
+        <option value='clapHappy'>Clap Your Hands!</option>
+        <option value='shoutHappy'>Shout Hooray!</option>
+        <option value='jumpHappy'>Jump for Joy!</option>
+      </select>
 
-            <select className='sadAct'> 
-                <option value='crySad'>Cry Boo-Hoo</option>
-                <option value='frownSad'>Show a Frown</option>
-                <option value='nappySad'>Take a Nap</option>
-            </select>
+      <select className='sadAct'>
+        <option value='0'>Select an Action!</option>  
+        <option value='crySad'>Cry Boo-Hoo</option>
+        <option value='frownSad'>Show a Frown</option>
+        <option value='nappySad'>Take a Nap</option>
+      </select>
 
-            <select className='angryAct'> 
-                <option value='tantrumAngry'>Throw Your Stuff!!</option>
-                <option value='yellAngry'>Scream and SHOUT!!</option>
-                <option value='slamAngry'>Slam the Door!!</option>
-            </select>
-        </div>
-    );
+      <select className='angryAct'> 
+        <option value='0'>Select an Action!</option> 
+        <option value='tantrumAngry'>Throw Your Stuff!!</option>
+        <option value='yellAngry'>Scream and SHOUT!!</option>
+        <option value='slamAngry'>Slam the Door!!</option>
+      </select>
+    </div>
+  );
 }
